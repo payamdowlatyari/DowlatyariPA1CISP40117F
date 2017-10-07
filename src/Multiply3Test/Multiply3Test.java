62a113fd3bed01a8555f65939033aa4945d5105f
Payam <pdowlatyari@gmail.com>
2017-10-06 19:35:22 -0700

    Initial commit

---
+++ src/Multiply3Test/Multiply3Test.java
@@ -0,0 +1,14 @@
+
+package Multiply3Test;
+
+public class Multiply3Test {
+
+    public static void main(String[] args) {
+       
+    Multiply3 object = new Multiply3();
+    
+        object.quiz();
+        object.printQuestionResult();
+      
+    }
+}
